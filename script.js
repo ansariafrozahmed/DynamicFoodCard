cafe37d6fff8bde9c8b661bfd879e779d97d16b9
Ansari Afroz Ahmed <137764964+ansariafrozahmed@users.noreply.github.com>
2023-09-08 03:42:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,71 @@
+let finals = [];
 const myFun = async () => {
-    try {
-        const url = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-    const result = await url.json()
-    const final = result.categories
-    document.getElementById('sec').innerHTML = final
-    .map((item) => 
+  try {
+    const url = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/categories.php"
+    );
+    const result = await url.json();
+    let final = result.categories;
+    finals.push(...final);
+  } catch (error) {
+    document.getElementById("errorsection").style = "display: block;";
+    console.log("ERROR" + " " + error);
+  }
+
+  document.getElementById("sec").innerHTML = finals.map(
+    (item) =>
+      `
+      <div class="card" id="card">
+              <img src="${item.strCategoryThumb}" alt="">
+              <h1 id="name">${item.strCategory}</h1>
+              <p id="desc">${item.strCategoryDescription.slice(0, 150)}</p>
+           </div>
+      `
+  );
+};
+
+
+const searchFun = () => {
+  const searchinput = document.getElementById("search");
+  const searchbtn = document.getElementById("searchbtn");
+  searchbtn.addEventListener("click", () => {
+    let a = finals.filter((single) => {
+      return (
+        single.strCategory.toUpperCase() === searchinput.value.toUpperCase()
+      );
+    });
+    document.getElementById("sec").innerHTML = a.map(
+      (item) =>
         `
         <div class="card" id="card">
                 <img src="${item.strCategoryThumb}" alt="">
                 <h1 id="name">${item.strCategory}</h1>
-                <p id="desc">${item.strCategoryDescription.slice(1, 150)}</p>
+                <p id="desc">${item.strCategoryDescription.slice(0, 150)}</p>
              </div>
         `
-    )
-    } catch (error) {
-        document.getElementById('errorsection').style = 'display: block;'
-        console.log('ERROR' +' '+ error);
+    );
 
-    }
+  });
+};
+
+
+const all = () => {
+  const allbtn = document.getElementById("allbtn");
+  allbtn.addEventListener('click', () => {
+    document.getElementById("sec").innerHTML = finals.map(
+      (item) =>
+        `
+        <div class="card" id="card">
+                <img src="${item.strCategoryThumb}" alt="">
+                <h1 id="name">${item.strCategory}</h1>
+                <p id="desc">${item.strCategoryDescription.slice(0, 150)}</p>
+             </div>
+        `
+    );
+  })
 }
 
+
 myFun();
+searchFun();
+all();
